0b7cc024c05412f75620a39223ce35120a7aa0b1
AkosuaSobolo <akosuaworde11@gmail.com>
2025-08-27 15:21:33 +0000

    show, add and delete for task manager

--- show.py
+++
@@ -1,4 +0,0 @@
-def show_tasks():
-    # Get all tasks fron database
-    # Return response
-    return []
--- update.py
+++
@@ -1,4 +0,0 @@
-def update_tasks(task, update):
-    # Update task in database
-    # Return response
-    return True
